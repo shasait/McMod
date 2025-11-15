80e4aad5927c88d8dd1e342be587c1f9cc3721ad
Sebastian Hasait <sebastian@hasait.de>
2025-11-15 12:08:47 +0100

    RepairGolemTask:  * fix broken logic for ticksToNextAction  * repair closest golem first

--- src/client/java/de/hasait/mcmod/McModClient.java
+++ src/client/java/de/hasait/mcmod/McModClient.java
@@ -1,10 +1,16 @@
 package de.hasait.mcmod;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class McModClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    public static final Logger LOGGER = LoggerFactory.getLogger(McMod.MOD_ID);
+
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+        LOGGER.info("onInitializeClient");
+    }
 }
--- src/main/java/de/hasait/mcmod/task/RepairGolemTask.java
+++ src/main/java/de/hasait/mcmod/task/RepairGolemTask.java
@@ -20,8 +20,14 @@ public class RepairGolemTask extends MultiTickTask<VillagerEntity> {
     private static final Map<EntityType<?>, Item> TARGET_TYPE_TO_ACTION_ITEM = Map.of(EntityType.IRON_GOLEM, Items.IRON_INGOT, EntityType.COPPER_GOLEM, Items.COPPER_INGOT);
     private static final float START_HEAL_IF_MAX_HEALTH_PERCENTAGE = 75.0F;
     private static final float HEAL_AMOUNT = 1.0F;
+    private static final double SCAN_X = 15.00;
+    private static final double SCAN_Y = 5.00;
+    private static final double SCAN_Z = 15.00;
+    private static final int TICKS_TO_NEXT_ACTION_BASE = 2;
+    private static final int TICKS_TO_NEXT_ACTION_RANDOM = 5;
     private GolemEntity target;
     private Item actionItem;
+    private int ticksToNextAction;
 
     public RepairGolemTask() {
         super(Map.of());
@@ -30,38 +36,33 @@ public class RepairGolemTask extends MultiTickTask<VillagerEntity> {
     @Override
     protected boolean shouldRun(ServerWorld world, VillagerEntity villager) {
         McMod.LOGGER.debug("RepairGolemTask.shouldRun");
-        List<GolemEntity> candidates = villager.getEntityWorld().getNonSpectatingEntities(GolemEntity.class, villager.getBoundingBox().expand(10.0D, 5.0D, 10.0D));
-        if (!candidates.isEmpty()) {
-            // prevent target re-assignment
-            if (target != null && candidates.contains(target)) {
-                return true;
-            }
-            for (GolemEntity candidate : candidates) {
-                float healthLimit = candidate.getMaxHealth() * START_HEAL_IF_MAX_HEALTH_PERCENTAGE / 100.0F;
-                Item candidateActionItem = determineActionItemIfSuitable(candidate, healthLimit);
-                if (candidateActionItem != null) {
+        List<GolemEntity> candidates = villager.getEntityWorld().getNonSpectatingEntities(GolemEntity.class, villager.getBoundingBox().expand(SCAN_X, SCAN_Y, SCAN_Z));
+        if (candidates.isEmpty()) {
+            return false;
+        }
+
+        // prevent target re-assignment
+        if (target != null && candidates.contains(target)) {
+            return true;
+        }
+
+        // select candidate closest to villager
+        Float minDistance = null;
+        for (GolemEntity candidate : candidates) {
+            float healthLimit = candidate.getMaxHealth() * START_HEAL_IF_MAX_HEALTH_PERCENTAGE / 100.0F;
+            Item candidateActionItem = determineActionItemIfSuitable(candidate, healthLimit);
+            if (candidateActionItem != null) {
+                float distance = villager.distanceTo(target);
+                if (minDistance == null || distance < minDistance) {
+                    minDistance = distance;
                     this.target = candidate;
                     this.actionItem = candidateActionItem;
-                    return true;
                 }
             }
         }
-        return false;
-    }
 
-    protected Item determineActionItemIfSuitable(GolemEntity candidate, Float optionalHealthLimit) {
-        McMod.LOGGER.debug("RepairGolemTask.determineActionItemIfSuitable: {}", candidate);
-        if (candidate == null) {
-            return null;
-        }
-        EntityType<?> type = candidate.getType();
-        float health = candidate.getHealth();
-        float healthLimit = optionalHealthLimit != null ? optionalHealthLimit : candidate.getMaxHealth();
-        McMod.LOGGER.debug("RepairGolemTask.determineActionItemIfSuitable: Type={} Health={}/{}", type, health, healthLimit);
-        if (!candidate.isAlive() || health >= healthLimit) {
-            return null;
-        }
-        return TARGET_TYPE_TO_ACTION_ITEM.get(type);
+        // minDistance is set if we found a candidate
+        return minDistance != null;
     }
 
     @Override
@@ -91,16 +92,34 @@ public class RepairGolemTask extends MultiTickTask<VillagerEntity> {
         actionItem = null;
     }
 
-    private void executeAction(VillagerEntity villager) {
-        McMod.LOGGER.debug("RepairGolemTask.executeAction");
+    protected void executeAction(VillagerEntity villager) {
+        McMod.LOGGER.debug("RepairGolemTask.executeAction: ticksToNextAction={}", ticksToNextAction);
         villager.getNavigation().startMovingTo(target, 0.5);
         if (villager.distanceTo(target) <= 2.0D) {
-            villager.equipStack(EquipmentSlot.MAINHAND, new ItemStack(actionItem));
-            villager.swingHand(Hand.MAIN_HAND);
-            target.heal(HEAL_AMOUNT);
-            float pitch = 5.0F + (target.getRandom().nextFloat() - target.getRandom().nextFloat()) * 0.2F;
-            target.playSound(SoundEvents.ENTITY_IRON_GOLEM_REPAIR, 0.5F, pitch);
+            if (ticksToNextAction-- <= 0) {
+                villager.equipStack(EquipmentSlot.MAINHAND, new ItemStack(actionItem));
+                villager.swingHand(Hand.MAIN_HAND);
+                target.heal(HEAL_AMOUNT);
+                float pitch = 5.0F + (target.getRandom().nextFloat() - target.getRandom().nextFloat()) * 0.2F;
+                target.playSound(SoundEvents.ENTITY_IRON_GOLEM_REPAIR, 0.5F, pitch);
+                ticksToNextAction = target.getRandom().nextInt(TICKS_TO_NEXT_ACTION_RANDOM) + TICKS_TO_NEXT_ACTION_BASE;
+            }
         }
+    }
+
+    protected Item determineActionItemIfSuitable(GolemEntity candidate, Float optionalHealthLimit) {
+        McMod.LOGGER.debug("RepairGolemTask.determineActionItemIfSuitable: {}", candidate);
+        if (candidate == null) {
+            return null;
+        }
+        EntityType<?> type = candidate.getType();
+        float health = candidate.getHealth();
+        float healthLimit = optionalHealthLimit != null ? optionalHealthLimit : candidate.getMaxHealth();
+        McMod.LOGGER.debug("RepairGolemTask.determineActionItemIfSuitable: Type={} Health={}/{}", type, health, healthLimit);
+        if (!candidate.isAlive() || health >= healthLimit) {
+            return null;
+        }
+        return TARGET_TYPE_TO_ACTION_ITEM.get(type);
     }
 
 }
